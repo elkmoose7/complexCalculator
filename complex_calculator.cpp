057a7e312c6568e3b60c30059f983439e633abd1
elkmoose7 <dinos131999@gmail.com>
2020-05-28 02:27:25 +0300

    Добавив ввід комплексного числа і його вивід

--- complex_calculator.cpp
+++ complex_calculator.cpp
@@ -10,5 +10,16 @@ int main()
     setlocale(LC_ALL, "ukr");
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
+
+    double cReal, cImag;
+
+    cout << "Введіть реальну частину частину" << endl;
+    cin >> cReal;
+    cout << "Введіть мниму частину числа" << endl;
+    cin >> cImag;
+
+    complex<double> cNumber(cReal, cImag);
+
+    cout << "Комплексне число " << cNumber << endl;
 }
 
