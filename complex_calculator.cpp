8d7634068247d4fbf47f696746549c86dca89b69
elkmoose7 <dinos131999@gmail.com>
2020-05-28 01:38:01 +0300

    Добавив українську локалізацію

--- complex_calculator.cpp
+++ complex_calculator.cpp
@@ -1,10 +1,14 @@
 ﻿#include <iostream>
 #include <complex>
+#include <locale>
+#include <Windows.h>
 
 using namespace std;
 
 int main()
 {
-    
+    setlocale(LC_ALL, "ukr");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
 }
 
